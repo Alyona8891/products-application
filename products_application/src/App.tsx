16c573f97d30aced43948a251f59a7da9fb27de0
Alyona <besssta888@mail.ru>
2023-10-28 15:16:08 +0300

    feat: add class App

--- products_application/src/App.tsx
+++ products_application/src/App.tsx
@@ -1,6 +1,36 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import styles from './App.module.scss';
 
-export function App(): React.ReactElement {
-  return <div>hi</div>;
+export class App extends Component {
+  state = {
+    inputValue: '',
+  };
+
+  handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target) {
+      this.setState({
+        inputValue: e.target.value,
+      });
+    }
+  };
+
+  handleSearchButton = () => {
+    alert(this.state.inputValue);
+  };
+
+  render() {
+    return (
+      <>
+        <header className={styles.header}>
+          <div className={styles.container}>
+            <input
+              value={this.state.inputValue}
+              onChange={this.handleInputChange}
+            />
+            <button onClick={this.handleSearchButton}>Search</button>
+          </div>
+        </header>
+      </>
+    );
+  }
 }
