e7e2c8234d1243be6e36ce2e3de6d5a566ce94c7
Alyona <besssta888@mail.ru>
2023-11-06 00:57:33 +0300

    feat: add NotFoundPage

--- products_application/src/components/router/router.tsx
+++ products_application/src/components/router/router.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { MainPage } from '../Pages/MainPage/MainPage';
 import { ErrorBoundary } from '../ErrorBoundary/ErrorBoundary';
+import { NotFoundPage } from '../Pages/NotFoundPage/NotFoundPage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
+    errorElement: <NotFoundPage />,
     children: [
       {
         path: '',
