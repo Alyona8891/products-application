98aa00a0dd489f6ada514347dca7d4921907b258
Alyona <besssta888@mail.ru>
2023-11-13 23:49:05 +0300

    feat: add testing Card.tsx

--- products_application/src/__tests__/Card.test.tsx
+++ products_application/src/__tests__/Card.test.tsx
@@ -1,23 +1,30 @@
-import { render, screen, fireEvent, act } from '@testing-library/react';
-import { expect, test, describe } from 'vitest';
+import {
+  render,
+  screen,
+  fireEvent,
+  waitFor,
+  cleanup,
+} from '@testing-library/react';
+import { expect, test, describe, vi, beforeEach, Mock } from 'vitest';
 import { Card } from '../components/Pages/MainPage/components/Card/Card';
 import { App } from '../components/App/App';
+import { IRequestResult } from '../types/types';
+import { mockProduct, mockRequestResult } from './mockData';
 
-function setTestingTimeout(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+global.fetch = vi.fn() as Mock;
+
+function createFetchResponse(data: IRequestResult) {
+  return { json: () => new Promise((resolve) => resolve(data)) };
 }
 
 describe('testing Card.tsx', () => {
-  test('displays that the card component renders the relevant card data', () => {
-    const product = {
-      id: 1,
-      title: 'dress',
-      text: 'for Woman',
-      images: ['for Woman'],
-      description: 'for Woman',
-    };
+  beforeEach((): void => {
+    cleanup();
+    (fetch as Mock).mockReset();
+  });
 
-    const card = render(<Card product={product} />);
+  test('displays that the card component renders the relevant card data', () => {
+    const card = render(<Card product={mockProduct} />);
     const title = card.getByText('dress');
     expect(title).toBeInTheDocument();
     const description = card.getByText('for Woman...');
@@ -25,16 +32,29 @@ describe('testing Card.tsx', () => {
   });
 
   test('clicking on a card opens a detailed card component', async () => {
-    act(() => {
-      render(<App />);
-    });
-    await act(() => setTestingTimeout(700));
+    (fetch as Mock).mockResolvedValue(createFetchResponse(mockRequestResult));
+    render(<App />);
+
     const cards = await screen.findAllByTestId('card');
     const card = cards[0];
     expect(screen.queryByTestId('details')).not.toBeInTheDocument();
     fireEvent.click(card);
-    await act(() => setTestingTimeout(700));
     await screen.findByTestId('details');
     expect(screen.getByTestId('details')).toBeInTheDocument();
+  });
+
+  test('clicking on a card triggers an additional API call to fetch detailed information', async () => {
+    (fetch as Mock).mockResolvedValue(createFetchResponse(mockRequestResult));
+    render(<App />);
+
+    await waitFor(() => {
+      expect(screen.getByText('iPhone 9')).toBeInTheDocument();
+    });
+    const cards = await screen.findAllByTestId('card');
+    const card = cards[0];
+    fireEvent.click(card);
+    await waitFor(() => {
+      expect(fetch).toHaveBeenCalledTimes(2);
+    });
   });
 });
---
+++ products_application/src/__tests__/mockData.ts
@@ -0,0 +1,34 @@
+export const mockRequestResult = {
+  total: 3,
+  products: [
+    {
+      id: 1,
+      title: 'iPhone 9',
+      text: '',
+      description: 'An apple mobile which is nothing like apple',
+      images: ['https://i.dummyjson.com/data/products/1/1.jpg'],
+    },
+    {
+      id: 2,
+      title: 'iPhone 10',
+      text: '',
+      description: 'An apple mobile which is nothing like apple',
+      images: ['https://i.dummyjson.com/data/products/1/1.jpg'],
+    },
+    {
+      id: 3,
+      title: 'iPhone 11',
+      text: '',
+      description: 'An apple mobile which is nothing like apple',
+      images: ['https://i.dummyjson.com/data/products/1/1.jpg'],
+    },
+  ],
+};
+
+export const mockProduct = {
+  id: 1,
+  title: 'dress',
+  text: 'for Woman',
+  images: ['for Woman'],
+  description: 'for Woman',
+};
