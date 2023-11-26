e561ae43fa0941f1326e23b877867d0b4c1234f0
Alyona <besssta888@mail.ru>
2023-11-26 11:30:10 +0300

    fix: fix error with head

--- products_application/src/pages/_app.tsx
+++ products_application/src/pages/_app.tsx
@@ -2,11 +2,15 @@ import type { AppProps } from 'next/app';
 import { wrapper } from '../components/store/store';
 import { Provider } from 'react-redux';
 import './globals.scss';
+import Head from 'next/head';
 
 export default function MyApp({ Component, ...rest }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(rest);
   return (
     <Provider store={store}>
+      <Head>
+        <title>Products Application</title>
+      </Head>
       <Component {...props.pageProps} />
     </Provider>
   );
--- products_application/src/pages/_document.tsx
+++ products_application/src/pages/_document.tsx
@@ -1,11 +1,10 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-        <title>Products Application</title>
       </Head>
       <body>
         <Main />
